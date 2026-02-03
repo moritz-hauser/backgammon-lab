3f6e7461cfcae8a44a9c428bbcaf8e355c133403
Moritz Hauser <a12327579@univie.ac.at>
2026-02-03 13:37:31 +0100

    Add configurable granularity for logging

--- src/bg_lab/__init__.py
+++ src/bg_lab/__init__.py
@@ -4,3 +4,6 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
+
+logging.getLogger('bg_lab.lab').setLevel(logging.DEBUG)
+logging.getLogger('bg_lab.arena').setLevel(logging.WARNING)
