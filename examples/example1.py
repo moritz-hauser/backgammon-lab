c176ac2e087f6998c003b7616daa20c6d1b5f859
Moritz Hauser <a12327579@univie.ac.at>
2026-02-08 10:42:38 +0100

    include catching MaxRoundsError in example

--- examples/example1.py
+++ examples/example1.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 from bg_game.game_state_model import GameStateModel
 from bg_view.cli_view import CliView
-from bg_game.game_controller import GameController
+from bg_game.game_controller import GameController, MaxRoundsError
 from bg_game.game_types import Color, WHITE, BLACK
 from bg_agents.random_agent import RandomAgent
 
@@ -14,7 +14,9 @@ gc = GameController(model)  # Manages match between two agents, updates the mode
 # Winner will be returned as color, so it is important to remember which agent has which color
 agents: dict[Color, RandomAgent] = {WHITE: RandomAgent(), BLACK: RandomAgent()}
 
-# Winner may be None if maximum amount of rounds is exceeded
-winner: Color = gc.compete(white_agent=agents[WHITE], black_agent=agents[BLACK])
-
-print(f"Winner: {winner} / {agents[winner]}")
+# GameController::compete throws in case the maximum of rounds is exceeded
+try:
+    winner: Color = gc.compete(white_agent=agents[WHITE], black_agent=agents[BLACK])
+    print(f"Winner: {winner} / {agents[winner]}")
+except MaxRoundsError as e:
+    print(f"Match terminated: {e}")
