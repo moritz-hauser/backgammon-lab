919864ea718e17c7647f0869f30c2a794b4aa431
Moritz Hauser <a12327579@univie.ac.at>
2026-02-09 18:03:49 +0100

    Change names to be more expressive

--- src/bg_game/game_state_model.py
+++ src/bg_game/game_state_model.py
@@ -5,8 +5,8 @@ from bg_game.game_types import (
     RoundSnapshot
     )
 
-GSObserver = Callable[[RoundSnapshot], None]
-ATObserver = Callable[[Action], None]
+SnapshotObserver = Callable[[RoundSnapshot], None]
+ActionObserver = Callable[[Action], None]
 WinnerObserver = Callable[[Optional[Color]], None]
 
 class GameStateModel:
@@ -14,45 +14,45 @@ class GameStateModel:
     Implements Observer-Model to be observerd by UI-Elements and such. 
     """
     def __init__(self):
-        self.gs: RoundSnapshot
-        self.at: Action
+        self.round_snapshot: RoundSnapshot
+        self.action_taken: Action
         self.winner: Optional[Color] = None
 
-        self.gs_observers: list[GSObserver] = []
-        self.at_observers: list[ATObserver] = []
+        self.snapshot_observers: list[SnapshotObserver] = []
+        self.action_observers: list[ActionObserver] = []
         self.winner_observers: list[WinnerObserver]= []
 
     # API for Controller
     def update_round_snapshot(self, round_snapshot: RoundSnapshot):
-        self.gs = round_snapshot
-        self._notify_gs_observers()
+        self.round_snapshot = round_snapshot
+        self._notify_snapshot_observers()
 
     def update_action_taken(self, action_taken: Action):
-        self.at = action_taken
-        self._notify_at_observers()
+        self.action_taken = action_taken
+        self._notify_action_observers()
 
     def update_winner(self, winner: Optional[Color]):
         self.winner = winner
         self._notify_winner_observers()
 
     # API for Observers
-    def on_new_round_snapshot(self, obs: GSObserver):
-        self.gs_observers.append(obs)
+    def on_new_round_snapshot(self, obs: SnapshotObserver):
+        self.snapshot_observers.append(obs)
 
-    def on_new_action_taken(self, obs: ATObserver):
-        self.at_observers.append(obs)
+    def on_new_action_taken(self, obs: ActionObserver):
+        self.action_observers.append(obs)
 
     def on_game_over(self, obs: WinnerObserver):
         self.winner_observers.append(obs)
 
     # Notify observers
-    def _notify_gs_observers(self):
-        for fn in self.gs_observers:
-            fn(self.gs)
+    def _notify_snapshot_observers(self):
+        for fn in self.snapshot_observers:
+            fn(self.round_snapshot)
 
-    def _notify_at_observers(self):
-        for fn in self.at_observers:
-            fn(self.at)
+    def _notify_action_observers(self):
+        for fn in self.action_observers:
+            fn(self.action_taken)
 
     def _notify_winner_observers(self):
         for fn in self.winner_observers:
