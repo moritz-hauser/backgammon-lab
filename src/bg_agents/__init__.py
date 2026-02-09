cec1d66f133bcf9d40ef99c96db2aac1ffe202b9
Moritz Hauser <a12327579@univie.ac.at>
2026-02-09 14:53:26 +0100

    Update log granularity

--- src/bg_agents/__init__.py
+++ src/bg_agents/__init__.py
@@ -1,6 +1,8 @@
 import logging
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.WARNING,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
+
+logging.getLogger('bg_agents.utility_based_agent').setLevel(logging.WARN)
