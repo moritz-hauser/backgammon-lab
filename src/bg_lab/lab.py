a9304df102555c437f72ee74242856ad7eef12c6
Moritz Hauser <a12327579@univie.ac.at>
2026-02-09 20:20:02 +0100

    Add base implementation of lab

--- src/bg_lab/lab.py
+++ src/bg_lab/lab.py
@@ -1,33 +1,76 @@
+from typing import TypeAlias
 from bg_game.game_controller import GameController
 from bg_agents.random_agent import RandomAgent
 from bg_agents.iagent import IAgent
+from bg_game.game_state_model import GameStateModel
 import logging
-import numpy as np
+from bg_lab.match_recorder import MatchRecorder, MatchRecording
+from bg_lab.imetric import IMetric, MetricResult
+
+MatchMetrics: TypeAlias = list[MetricResult]
 
 log = logging.getLogger(__name__) 
 
+class DuplicateMetricIdError(RuntimeError):
+    pass
+
 class Lab:
 
-    def __init__(self, arena: GameController, replayer: GameReplayer, analyzer: GameReplayAnalyzer):
-        self.arena = arena
-        self.replayer = replayer
-        self.analyzer = analyzer
+    # Allow caller to use his own model, in case of desired cli output
+    def __init__(self):
+        self.metrics: dict[str, IMetric] = {}  # id -> metric
+        for metric in self._default_metrics():
+            self.add_metric(metric)
+    
+    @classmethod
+    def _default_metrics(cls) -> list[IMetric]:
+        return [] # TODO: Add default metrics here
+    
+    @classmethod
+    def _check_metric_ids_unique(cls, metrics: list[IMetric]) -> bool:
+        ids = [metric.id for metric in metrics]
+        return len(ids) == len(set(ids))
 
-    def match_up(self, agent1, agent2):
-        log.info("Called method match_up().")
-        return self.arena.compete(agent1, agent2) 
-
-    def compare_agents(self, agent1: IAgent, agent2: IAgent, metrics: List[IMetric], n_matches=100):
+    
+    def add_metric(self, metric: IMetric):
+        if metric.id in self.metrics:
+            raise DuplicateMetricIdError(
+                f"Metric-ID '{metric.id}' is already in use. "
+                f"Unavailable IDs: {', '.join(sorted(self.metrics.keys()))}"
+            )
+        self.metrics[metric.id] = metric
+    
+    def compare_agents(
+            self, 
+            white_agent: IAgent, 
+            black_agent: IAgent, 
+            n_matches: int=100
+        ) -> list[MatchMetrics]:
         log.info("Called method compare agents.")
 
-        traces = [self.arena.compete(agent1, agent2) for _ in range(n_matches)]
+        all_matches_data: list[MatchMetrics] = []
 
-        log.info(f"Recorded {len(traces)} matches.")
+        for i in range(n_matches):
+            # Avoid hidden state dependencies 
+            model = GameStateModel()
+            gc = GameController(model)
+            recorder = MatchRecorder(model)
 
-        game_reports = np.empty(len(traces))
-        for i, trace in enumerate(traces):
-            log.debug(f"Running analysis {i}/{len(traces)}")
-            replay = self.replayer.replay_game(trace)
-            game_reports[i] = self.analyzer.analyze(replay)
+            gc.compete(white_agent=white_agent, black_agent=black_agent)
 
-        return aggregate_reports(game_reports)
+            recording: MatchRecording = recorder.get_recording()
+
+            # Analyze recording
+            match_metrics: MatchMetrics = []
+            for metric in self.metrics.values():
+                metric_result: MetricResult = metric.analyze(recording)
+                match_metrics.append(metric_result)
+
+            all_matches_data.append(match_metrics)
+
+        return all_matches_data
+
+
+
+
+        
