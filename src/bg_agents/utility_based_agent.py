fc24c24739b7a5729ad086aa07abf5ee5a5b8c88
Moritz Hauser <a12327579@univie.ac.at>
2026-02-09 14:40:46 +0100

    Add simple utility based agent

--- src/bg_agents/utility_based_agent.py
+++ src/bg_agents/utility_based_agent.py
@@ -3,19 +3,38 @@ from bg_agents.iagent import IAgent
 from bg_game.game_types import (
     AgentPerspectiveState, Action,
 )
+import logging
 
 """
 Utility based agent with one-step lookahead.
 (Greedy)
 """
 
+log = logging.getLogger(__name__) 
+
 class UtilityBasedAgent(IAgent):
 
     def _utility(self, state: AgentPerspectiveState) -> int:
-        raise NotImplementedError("Utility function has not been implemented")
-    
-    def _transition(self, state: AgentPerspectiveState, action: Action) -> AgentPerspectiveState:
-        raise NotImplementedError("Result function has not been implemented.")
+        #raise NotImplementedError("Utility function has not been implemented")
+        utility: int = 0
+
+        # Increase utility for hit enemies
+        utility += state.bar_enemy
+
+        # Increase utility for off moves
+        utility += 2 * state.off_me
+
+        for amount_checkers in state.points:
+            match amount_checkers:
+                case 1: 
+                    utility -= 1    # Decrease utility for blops
+                case 2: 
+                    utility += 1    # Prefer small stacks over large ones
+                case 3:
+                    utility += 2    # Stack of 3 considered optimal (for flexibility)
+
+        return utility
+
     
     def choose_action(self, state: AgentPerspectiveState, actions: list[Action]) -> Action:
         """
@@ -25,13 +44,19 @@ class UtilityBasedAgent(IAgent):
         assert actions, "Received empty list of actions."
 
         best_action: Optional[Action] = None
-        best_utility = 0
+        best_utility = -100_000 # very small
+
         for action in actions:
             result_state = self.transition_model.result(state, action)
+
             utility = self._utility(result_state)
+
             if utility > best_utility:
                 best_action = action
                 best_utility = utility
 
         assert best_action is not None
+
+        log.info(f"Highest utility ({best_utility}) found for action {best_action}.")
+
         return best_action
